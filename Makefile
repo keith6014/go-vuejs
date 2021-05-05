8ee30bd34f1ecbcd93cac404fe5c647a9036a1b9
Your Name <you@example.com>
2021-05-05 09:46:56 -0400

    possibly final commit

--- Makefile
+++ Makefile
@@ -5,17 +5,19 @@ all: server
 
 proto/helloworld/*.go: proto/helloworld/helloworld.proto
 	bin/protoc -I ./proto \
+		-I /home/user/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/v2@v2.4.0/ \
 		--go_out ./proto --go_opt paths=source_relative \
 		--go-grpc_out ./proto --go-grpc_opt paths=source_relative \
 		--grpc-gateway_out ./proto --grpc-gateway_opt paths=source_relative \
+		--swagger_out=logtostderr=true:. \
 		proto/helloworld/helloworld.proto
 
 main: cmd/main.go
 	go build $^
 
-server: proto/helloworld/*.go | cmd/server.go
+server: proto/helloworld/*.go cmd/server.go
 	go build cmd/server.go
 
 clean:
-	rm -rf main server proto/helloworld/*.go
+	rm -rf main server proto/helloworld/*.go helloworld/*.json
 .PHONY: clean
--- cmd/server.go
+++ cmd/server.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"embed"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"io/fs"
 	"log"
 	"net"
 	"net/http"
 
 	pb "go-vuejs/proto/helloworld"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
@@ -27,6 +34,12 @@ func NewServer() *server {
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: in.Name + " world"}, nil
 }
+
+//go:embed "helloworld/helloworld.swagger.json"
+var myjson string
+
+//go:embed "helloworld/swagger-ui-3.47.1/dist"
+var content embed.FS
 
 func main() {
 	log.Println("grpc server on", port)
@@ -49,16 +62,57 @@ func main() {
 		log.Fatal("err ", err)
 	}
 	gwmux := runtime.NewServeMux()
+
 	err = pb.RegisterGreeterHandler(context.Background(), gwmux, conn)
 	if err != nil {
 		log.Fatalln("Failed to register gateway:", err)
 	}
 
-	gwServer := &http.Server{
-		Addr:    ":8080",
-		Handler: gwmux,
-	}
+	//	gwServer := &http.Server{
+	//		Addr:    ":8080",
+	//		Handler: gwmux,
+	//	}
+
+	//fs := http.FileServer(http.Dir("./helloworld"))
+	//	err = gwmux.HandlePath("GET", "/b", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+	//		w.Write([]byte("hello " + pathParams["name"]))
+	//	})
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	//	mux := http.NewServeMux()
+	//	mux.Handle("/", gwmux)
+	//	mux.Handle("/helloworld/", http.StripPrefix("/helloworld/", fs))
+	//	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+	//		io.WriteString(w, string(myjson))
+	//	})
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Handle("/", gwmux)
+	r.HandleFunc("/api/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		b, err := json.Marshal(myjson)
+		if err != nil {
+			http.Error(w, err.Error(), 422)
+		}
+		w.Write(b)
+	})
+	r.Handle("/d", clientHandler())
 
 	log.Println("Starting up webserver on :8080")
-	log.Fatalln(gwServer.ListenAndServe())
+	//log.Fatalln(gwServer.ListenAndServe())
+	log.Println(http.ListenAndServe(":8080", r))
+}
+
+func clientHandler() http.Handler {
+	fsys := fs.FS(content)
+	contentStatic, err := fs.Sub(fsys, "index.html")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(contentStatic)
+	return http.FileServer(http.FS(contentStatic))
 }
